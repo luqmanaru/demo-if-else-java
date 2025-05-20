7f8948a85680f048584369decca8938293d6d375
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-20 08:40:08 +0700

    Create demo-if-else.java

---
+++ demo-if-else.java
@@ -0,0 +1,18 @@
+package com.mycompany.strukturkontrolpemilihan;
+
+/**
+ * Demonstrasi Struktur Kontrol Pemilihan (if-else)
+ * @author luqmanaru
+ */
+public class StrukturKontrolPemilihan {
+    public static void main(String[] args) {
+        int grade = 68; // Nilai default
+        
+        // Struktur kontrol if-else
+        if (grade > 60) {
+            System.out.println("YEY NGGAA NGULANG!");
+        } else {
+            System.out.println("YAH NGULANG LAGI BRO 1");
+        }
+    }
+}
